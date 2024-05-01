80efff606cc988e45c144f4c963e539aeed0645d
Родион <167028104+GalievRodion@users.noreply.github.com>
2024-05-01 19:22:02 +0300

    Add files via upload
    Домашняя работа по уроку "Функции в Python.Функция с параметром". Попытка №2

--- homework2_5.py
+++ homework2_5.py
@@ -1,5 +1,7 @@
-def print_paras():
-    print('Во что веришь, то и есть. Максим Горький ')
 
-print_paras()
-print_paras()
+def print_paras(name, online_university):
+    print('Привет, меня зовут ' + name + '!')
+    print('Я учусь в онлайн-университете ' + online_university + '.')
+
+print_paras('Антон', 'Urban')
+print_paras('Света', 'Яндекс практикум')
